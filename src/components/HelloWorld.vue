458501b5853914ebbf89dda957fe93e6ac2bd19b
ed42311 <14878694+ed42311@users.noreply.github.com>
2019-07-15 23:22:09 -0600

    setting up baseline

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,18 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <a href="">Github front-end</a> |
+      <a href="https://github.com/latovicalmin/nodejs-websockets" target="_blank">Github back-end</a>
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h1>{{ msg }}</h1>
+    <span @click="connect" style="color: green; cursor: pointer">Connect websockets</span> |
+    <span @click="disconnect" style="color: red; cursor: pointer">Disconnect websockets</span> |
+    <span @click="send" style="color: blue; cursor: pointer">Send</span>
+    <h1>Received events:</h1>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(notification, index) in notififications" :key="index">
+        {{ notification }}
+      </li>
     </ul>
   </div>
 </template>
@@ -33,16 +20,35 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      msg: 'Vue.js websockets basic example'
+    }
+  },
+  computed: {
+    notififications () {
+      return this.$store.getters['notifications/getNotifications']
+    }
+  },
+  methods: {
+    connect () {
+      this.$webSocketsConnect()
+    },
+    disconnect () {
+      this.$webSocketsDisconnect()
+    },
+    send () {
+      let d = new Date() + ' hello'
+      this.$webSocketsSend(d)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+h1, h2 {
+  font-weight: normal;
 }
 ul {
   list-style-type: none;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import webSocketService from './services/webSocketService'
+import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(webSocketService, {
+  store,
+  url: 'ws://localhost:8081'
+})
+
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  store
+})
